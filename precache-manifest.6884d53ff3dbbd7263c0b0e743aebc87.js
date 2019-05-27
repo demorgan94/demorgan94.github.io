ba5a86ce9032157b8c1d7ceeb929c91f3833c3e0
David Morgan <demorgan94@gmail.com>
2019-05-27 12:57:09 -0700

    Primer Commit

---
+++ precache-manifest.6884d53ff3dbbd7263c0b0e743aebc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb73fe4ca4d8540e5e241a79ba84f59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82e8d35f8ee583f6fead",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "b87cfd29211354c04ebe",
+    "url": "/static/css/main.8463a831.chunk.css"
+  },
+  {
+    "revision": "82e8d35f8ee583f6fead",
+    "url": "/static/js/2.37424e8c.chunk.js"
+  },
+  {
+    "revision": "b87cfd29211354c04ebe",
+    "url": "/static/js/main.bc8d9d91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
